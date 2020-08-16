4679674140336700975178725515a916d297eecd
EkkoYu <17712427671@163.com>
2020-08-16 16:50:15 +0800

    提交到本地仓库

---
+++ src/main/java/GitDemo.java
@@ -0,0 +1,15 @@
+/**
+ * @Author yuSJie
+ * @Date 2020/8/16 16:45
+ * @Version 1.0
+ * Git示例
+ */
+public class GitDemo {
+
+
+    public void add(){
+
+        System.out.println("Hello Git");
+
+    }
+}
